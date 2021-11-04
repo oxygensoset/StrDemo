6da03bca5268e0fa18da3299fcc832413a6c3e20
oxygensoset <saveliy.burmistrov.99@mail.ru>
2021-11-04 15:54:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// для перевода каретки  (нчало новой строки)
+     //  служит код \n
+     System.out.println("Первая строка\nВторая строка");
+
+     //знак табуляции помогает оформить выводимый текст
+        // в колонки. Он вводится с помощью кода \t
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    } //main(String[]) method
+} // StrDemo class
